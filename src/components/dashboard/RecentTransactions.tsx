704574a3f90fbba3b24ac7e30a1372750215b20a
Cito2310 <cito2310@gmail.com>
2024-11-28 11:51:40 -0300

    added recentTransactions to dashboard styled

--- src/components/dashboard/RecentTransactions.tsx
+++ src/components/dashboard/RecentTransactions.tsx
@@ -1,9 +1,75 @@
 import { ContainerDashboard } from "./ContainerDashboard"
 
+interface propsChildren {
+    children?: JSX.Element | (JSX.Element|string)[] | string;
+    className?: string;
+}
+
+interface propsLine {
+    data: {
+        name: string;
+        account: string;
+        date: string;
+        amount: string;
+        category: string;
+    };
+}
+
+export const TranstactionsTh = ({children, className}:propsChildren) => (
+    <th className={`text-[#8b9199] font-normal text-left ${className}`}>
+        {children}
+    </th>
+)
+
+export const TranstactionCell = ({children, className}:propsChildren) =>
+    <td className={`text-[#444] font-normal border-collapse border-y-[1em] border-transparent ${className}`}>{children}</td>
+
+export const TransactionsLine = ({data}:propsLine) => (
+    <tr>
+        <TranstactionCell>
+            <div>
+                <p>{data.name}</p>
+                <p className="text-[#8b9199]">{data.category}</p>
+            </div>
+        </TranstactionCell>
+        <TranstactionCell>{data.date}</TranstactionCell>
+        <TranstactionCell className="text-right">{data.amount}</TranstactionCell>
+    </tr>
+)
+
+
 export const RecentTransactions = () => {
     return (
         <ContainerDashboard>
-            <h2 className="font-semibold text-black font-Montserrat text-xl mb-2">Transacciones Recientes</h2>
+            <h2 className="font-semibold text-black font-Montserrat text-xl">Transacciones Recientes</h2>
+
+            <table className="table-fixed w-full">
+            <tbody>
+                <TransactionsLine data={{
+                    account:"Platinum",
+                    amount: "+ $1.240.41",
+                    category: "Income",
+                    date: "08 Aug 2023 5:02 AM",
+                    name: "Paypal"
+                }}/>
+                <TransactionsLine data={{
+                    account:"Regular",
+                    amount: "- $15,49",
+                    category: "Entertainment",
+                    date: "08 Aug 2023 2:16 PM",
+                    name: "Netflix"
+                }}/>
+                <TransactionsLine data={{
+                    account:"Platinum",
+                    amount: "- $9,72",
+                    category: "Education",
+                    date: "07 Aug 2023 6:01 PM",
+                    name: "Notion"
+                }}/>
+            </tbody>
+
+            <tfoot><tr></tr></tfoot>
+        </table>
         </ContainerDashboard>
     )
 }
